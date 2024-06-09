40d5f16b3c6368aba629c7761f3001dacab87ab6
Andres Diago <andresdiag@unicauca.edu.co>
2024-06-09 15:59:52 -0500

    :construction: Clase 21. Usando effect() para localstorage

--- src/app/website/pages/home/home.component.ts
+++ src/app/website/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, signal, ElementRef, computed } from '@angular/core'
+import { Component, HostListener, signal, ElementRef, computed, effect, Injector, inject } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { Task } from '../../../models/task.model'
@@ -16,36 +16,36 @@ type FilterValues = 'all' | 'completed' | 'pending'
 export class HomeComponent {
   // --------- Properties ---------
   tasks = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'First Task',
-      description: 'This is the first task',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Second Task',
-      description: 'This is the second task',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Third Task',
-      description: 'This is the third task',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Correr',
-      description: 'Correr en la mañana',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Leer',
-      description: 'Correr en la mañana',
-      completed: true
-    }
+    // {
+    //   id: Date.now(),
+    //   title: 'First Task',
+    //   description: 'This is the first task',
+    //   completed: true
+    // },
+    // {
+    //   id: Date.now(),
+    //   title: 'Second Task',
+    //   description: 'This is the second task',
+    //   completed: false
+    // },
+    // {
+    //   id: Date.now(),
+    //   title: 'Third Task',
+    //   description: 'This is the third task',
+    //   completed: false
+    // },
+    // {
+    //   id: Date.now(),
+    //   title: 'Correr',
+    //   description: 'Correr en la mañana',
+    //   completed: false
+    // },
+    // {
+    //   id: Date.now(),
+    //   title: 'Leer',
+    //   description: 'Correr en la mañana',
+    //   completed: true
+    // }
   ])
   editingItem: Task | null = null
   newTaskControl = new FormControl('', {
@@ -66,10 +66,30 @@ export class HomeComponent {
       return tasks
     }
   })
+  filteredTasksCount = computed(() => {
+    return this.filteredTasks().length || 0
+  })
 
   // --------- Constructor ---------
 
   constructor(private eRef: ElementRef) {}
+
+  ngOnInit() {
+    const tasks = localStorage.getItem('tasks')
+    if (tasks) {
+      console.log('Tasks loaded from local storage') // Debugging line
+      this.tasks.set(JSON.parse(tasks))
+    }
+    this.trackTasks()
+  }
+
+  injector = inject(Injector)
+  trackTasks(){
+    effect(() => {
+      const tasks = this.tasks()
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, { injector: this.injector })
+  }
 
   // --------- Methods ---------
   addTask() {
